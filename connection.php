d5555a669a1de61c5da4d656ae7af16bc4b96125
amberyeoe <104883642+amberyeoe@users.noreply.github.com>
2023-11-16 02:17:56 +0900

    connection function

--- connection.php
+++ connection.php
@@ -35,6 +35,21 @@ class DatabaseConnection {
         $row=$results->fetch_assoc();
         return $row;
     }
+
+    function createQuizTable($quiz_no){
+        $con=mysqli_connect('localhost','root','','webdb_project');
+        $table_query="create table quiz_".$quiz_no."(
+            quiz_no INT PRIMARY KEY AUTO_INCREMENT, 
+            question VARCHAR(255) NOT NULL, 
+            option_1 VARCHAR(255) NOT NULL, 
+            option_2 VARCHAR(255) NOT NULL, 
+            option_3 VARCHAR(255) NOT NULL, 
+            option_4 VARCHAR(255) NOT NULL, 
+            answer VARCHAR(255) NOT NULL
+        )";
+        mysqli_query($con,$table_query);
+    }
+
 }
 
 ?>
