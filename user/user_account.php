765ce710b6e22e3f7cfea4c372b6f46e4cd805d5
Jae <90853880+javerinetan@users.noreply.github.com>
2023-11-15 23:02:25 +0800

    Update user_account.php

--- user/user_account.php
+++ user/user_account.php
@@ -1,6 +1,43 @@
+<!-- for log in only function -->
 <?php 
 require_once('../connection.php');
 session_start(); 
+if(!isset($_SESSION['User']))
+    {header("location:login.php");}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title></title>
+
+    <!-- bootstraps -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+    crossorigin="anonymous"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../wf/style.css" rel="stylesheet" type="text/css" />
+
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></script>
+
+    <link href="../wf/assets/QuizITLogo.png" rel="shortcut icon" type="image/x-icon" />
+    <link href="../wf/assets/QuizITLogo.png" rel="apple-touch-icon" />
+
+
+</head>
+
+<body>
+<?php  
 if(isset($_SESSION['User']))
     {
         $query="select * from account where email='".$_SESSION['User']."'";
@@ -21,3 +58,9 @@ else
         header("location:login.php");
     }
 ?>
+
+
+</body>
+
+<footer>
+
