d726076d8efc45dbfd96f20014d6509039f39715
Jae <90853880+javerinetan@users.noreply.github.com>
2023-11-16 14:50:11 +0800

    Update delete_user.php
    i ==> integer

--- user/delete_user.php
+++ user/delete_user.php
@@ -4,20 +4,26 @@ session_start();
 
 if (!isset($_SESSION['User'])) {
     header("location:../account/login.php");
+    exit;
 } else {
     // Use prepared statement to prevent SQL injection
-    $query = "DELETE FROM account WHERE email=?";
+    $query = "DELETE FROM account WHERE id=?";
     $stmt = $con->prepare($query);
-    $stmt->bind_param("s", strtolower($_SESSION['User'])); // 's' indicates a string parameter
+
+    // Assuming id is an integer, use 'i' as the data type specifier
+    $stmt->bind_param("i", $_SESSION['User']); 
+
     $stmt->execute();
 
     // Check if the deletion was successful
     if ($stmt->affected_rows > 0) {
         session_destroy();
         header('location:../index.php');
+        exit;
     } else {
         // Handle the case where no rows were affected (user not found, or other issues)
         echo "Error: User not deleted.";
+        exit;
     }
 
     $stmt->close();
