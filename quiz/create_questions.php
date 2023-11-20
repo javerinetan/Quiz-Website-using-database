5b982e0b6123c170c86aa7b29187c29c5b117122
Jae <90853880+javerinetan@users.noreply.github.com>
2023-11-21 03:34:18 +0800

    Created the Quiz page
    Issue --> the last questions is not reflecting correctly

--- quiz/create_questions.php
+++ quiz/create_questions.php
@@ -205,60 +205,8 @@ if(!isset($_SESSION['User']))
 
 <script src="https://proxy-translator.app.crowdin.net/assets/proxy-translator.js"></script>
 <script src='../language.js'></script>
+<script src="quiz.js"></script>
 
-<script>
-
-    $(document).ready(function () {
-                $("#quiz-form").submit(function () {
-                    var inputs = $("input[type='text']", this);
-                    for (var i = 0; i < inputs.length; i++) {
-                        if (inputs[i].value.trim() === '') {
-                            alert('Please fill in all the input fields.');
-                            return false;
-                        }
-                    }
-                    return true;
-                });
-            });
-
-    document.addEventListener('DOMContentLoaded', function () {
-        var questionContainers = document.querySelectorAll('.question-container');
-
-        function showQuestion(questionNumber) {
-            questionContainers.forEach(function (container, index) {
-                if (index + 1 === questionNumber) {
-                    container.style.display = 'block';
-                } else {
-                    container.style.display = 'none';
-                }
-            });
-        }
-
-        var questionLinks = document.querySelectorAll('.sidebar ul li a');
-        questionLinks.forEach(function (link) {
-            link.addEventListener('click', function (event) {
-                event.preventDefault();
-                var questionNumber = parseInt(this.textContent.match(/\d+/)[0], 10);
-                showQuestion(questionNumber);
-            });
-        });
-
-        // Show the first question by default
-        showQuestion(1);
-    });
-
-    function submitForm() {
-            // Check if the form is valid before submitting
-            if (document.getElementById('quizForm').checkValidity()) {
-                // Add your code to submit the form
-                console.log('Form submitted!');
-            } else {
-                alert('Please fill in all the required fields.');
-            }
-        }
-
-
-</script>
 
 </footer>
 
--- quiz/quiz.php
+++ quiz/quiz.php
@@ -1,10 +1,35 @@
-<!-- for log in only function -->
-<?php 
-require_once('../connection.php'); 
-session_start(); 
-if(!isset($_SESSION['User']))
-    {header("location:../account/login.php");} 
+<?php
+require_once('../connection.php');
+session_start();
+if (!isset($_SESSION['User'])) {
+    header("location:../account/login.php");
+}
+
+// Assuming you have the quiz_id in the URL parameter
+if (isset($_GET['quiz_id'])) {
+    $quiz_id = $_GET['quiz_id'];
+
+    // Fetch quiz details
+    $quiz_query = "SELECT * FROM quiz WHERE quiz_id = $quiz_id";
+    $quiz_result = mysqli_query($con, $quiz_query);
+    $quiz_row = mysqli_fetch_assoc($quiz_result);
+
+    if ($quiz_row) {
+        $quiz_name = $quiz_row['quiz_name'];
+
+        // Fetch quiz questions
+        $questions_query = "SELECT * FROM quiz_$quiz_id";
+        $questions_result = mysqli_query($con, $questions_query);
+        $questions = mysqli_fetch_all($questions_result, MYSQLI_ASSOC);
+
+        // Shuffle the options for each question
+        foreach ($questions as &$question) {
+            $options = array($question['option_1'], $question['option_2'], $question['option_3'], $question['option_4']);
+            shuffle($options);
+            $question['options'] = $options;
+        }
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -17,7 +42,7 @@ if(!isset($_SESSION['User']))
     <!-- bootstraps -->
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="../user/userstyle.css">
+    <link rel="stylesheet" href="quiz_style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
@@ -44,7 +69,65 @@ if(!isset($_SESSION['User']))
 
 
 <body>
-    
+
+<main class="containers">
+    <div class="header text-center">
+        <h1 class="display-4 Quiz_name"><?php echo $quiz_name; ?> Quiz</h1>
+    </div>
+
+    <div class="sidebar">
+        <h2>Questions</h2>
+        <ul>
+            <?php
+                foreach ($questions as $question) {
+                    echo '<li><a href="#question' . $question['question'] . '">Question ' . $question['quiz_no'] . '</a></li>';
+                }
+            ?>
+        </ul>
+    </div>
+
+    <div class="main-content">
+        <?php
+        foreach ($questions as $question) {
+            echo '
+            <div class="question-container" id="question' . $question['question'] . '">
+                <h3>Question ' . $question['quiz_no'] . '</h3>
+                <p>' . $question['question'] . '</p>
+                <form method="post" action="process_quiz.php">
+                    <input type="hidden" name="question_id" value="' . $question['question'] . '">
+                    <input type="hidden" name="quiz_id" value="' . $quiz_id . '">
+                    <ul style="list-style-type: none; padding: 0;">';
+            foreach ($question['options'] as $option) {
+                echo '
+                        <li>
+                            <label>
+                                <input type="radio" name="answer" value="' . $option . '" required>
+                                ' . $option . '
+                            </label>
+                        </li>';
+            }
+            echo '
+                    </ul>
+                </form>
+            </div>';
+        }
+        ?>
+</div> 
+
+</main>
+
+    <?php
+    } else {
+        echo "Quiz not found!";
+    }
+    } else {
+    echo "Quiz ID not provided!";
+    }
+    ?>
+
+
+
+
 </body>
 
 <footer>
@@ -52,6 +135,7 @@ if(!isset($_SESSION['User']))
 
 <script src="https://proxy-translator.app.crowdin.net/assets/proxy-translator.js"></script>
 <script src='../language.js'></script>
+<script src="quiz.js"></script>
 </footer>
 
 </html>
