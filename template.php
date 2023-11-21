d1b436236052f033c939271b938666f9ea09fe8f
amberyeoe <104883642+amberyeoe@users.noreply.github.com>
2023-11-21 16:31:47 +0900

    remove bottom script

--- template.php
+++ template.php
@@ -25,7 +25,6 @@ if(!isset($_SESSION['User']))
 
 
 <footer>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
 <script src="https://proxy-translator.app.crowdin.net/assets/proxy-translator.js"></script>
 <script src='../language.js'></script>
 </footer>
