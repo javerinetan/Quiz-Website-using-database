2b298117a7c64ddcfd7d4e65c0c3aeef5b2e53ff
amberyeoe <104883642+amberyeoe@users.noreply.github.com>
2023-11-22 00:14:58 +0900

    navbar

--- quiz/view_quiz.php
+++ quiz/view_quiz.php
@@ -23,106 +23,12 @@ if(!isset($_SESSION['User']))
 
     </title>
 
-    <!-- bootstraps -->
-    <link rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="../user/userstyle.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-    crossorigin="anonymous"></script>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.bundle.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></script>
-
-    <link href="../wf/assets/QuizITLogo.png" rel="shortcut icon" type="image/x-icon" />
-    <link href="../wf/assets/QuizITLogo.png" rel="apple-touch-icon" />
-
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-
     <style>
-        #container-central{
-         width: 70%;
-         height: 30%;   
-         margin-left: auto;
-         margin-right: auto;
-        }
+        
     </style>
 
 </head>
-<nav class="navbar navbar-expand-lg navbar-light">
-    <div class="container-fluid">
-        <!-- Logo and Search Bar on the left -->
-        <img class="navbar-brand" src="../wf/assets/QuizITLogoname.png" href="user_dashboard.php">
-
-        <!-- Toggler Button for small screens -->
-        <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="toggler-icon top-bar"></span>
-            <span class="toggler-icon middle-bar"></span>
-            <span class="toggler-icon bottom-bar"></span>
-        </button>
-        <form class="d-flex">
-            <input class="form-control me-1" type="search" placeholder="Search" aria-label="Search">
-        </form>
-
-        <!-- Navbar items at the center -->
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="../user/home.php">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Activity</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="dashboard.php" tabindex="-1" aria-disabled="true">Dashboard</a>
-                </li>
-            </ul>
-        </div>
-
-        <!-- Profile button and dropdown on the left -->
-        <div class="dropdown">
-        <a href="../quiz/create_quiz.php"><button class="btn create " type="submit" href="create_quiz.php">Create a Quiz</button></a>
-
-            <!-- Profile dropdown with image -->
-            <button class="btn profile dropdown-toggle" type="button" id="profileDropdown" data-bs-toggle="dropdown" aria-expanded="false">
-                <img src="../wf/assets/dd.jpg" alt="User Image" class="user-image">
-            </button>
-            <ul class="profile_drop dropdown-menu" aria-labelledby="profileDropdown">
-                <li class="details_user">
-                <?php  
-                if(isset($_SESSION['User']))
-                    {
-                        $query="select * from account where id=".$_SESSION['User']."";
-                        $results=mysqli_query($con,$query);
-                        $row=$results->fetch_assoc();
-                        echo $row['name'];
-                        echo '<br>';
-                        echo $row['email'];
-                    }
-                else
-                    {
-                        header("location:login.php");
-                    }
-                ?>
-                </li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item" href="user_account.php">Settings</a></li>
-                <li><a class="dropdown-item" href="../account/logout.php?logout">Log Out</a>
-                
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
-
+<?php require '../navbar.php' ?>
 
 </head>
 
@@ -156,6 +62,12 @@ if(!isset($_SESSION['User']))
  
         td {
             font-weight: lighter;
+        }
+        #container-central{
+         width: 70%;
+         height: 30%;   
+         margin-left: auto;
+         margin-right: auto;
         }
 </style>
 
